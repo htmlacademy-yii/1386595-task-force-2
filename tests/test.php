1faeac047f8ffe1ab7dea2669378df19ed930c2c
Ольга Н. Курбатова <kon@2i.tusur.ru>
2022-02-08 16:18:25 +0700

    добавление тестового скрипта

--- tests/test.php
+++ tests/test.php
@@ -1,3 +1,11 @@
 <?php
 
-require '/models/Task.php';
+require '../models/Task.php';
+
+$testTask = new Task(503, 354);
+echo "Проверка: getNewStatus(Task::ACTION_COMPLETE) <br>Ожидаем: выполнено<br>Результат: ";
+echo $testTask->getNewStatus(Task::ACTION_COMPLETE);
+
+//echo $testTask->getAvailableActions(Task::STATUS_NEW);
+//echo $testTask->getStatusesMap();
+//echo $testTask->getActionsMap();
