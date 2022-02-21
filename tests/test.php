8aeaad9de82ce9fd9f9f9855c95d59571929b0e9
Ольга Н. Курбатова <kon@2i.tusur.ru>
2022-02-21 13:18:03 +0700

    добавление проверки метода getAvailableActions

--- tests/test.php
+++ tests/test.php
@@ -5,7 +5,13 @@ require '../models/Task.php';
 $testTask = new Task(503, 354);
 echo "Проверка: getNewStatus(Task::ACTION_COMPLETE) <br>Ожидаем: completed<br>Результат: ";
 echo $testTask->getNewStatus(Task::ACTION_COMPLETE);
+echo "<br>";
+echo "Проверка: getNewStatus(Task::ACTION_CANCEL) <br>Ожидаем: cancelled<br>Результат: ";
+echo $testTask->getNewStatus(Task::ACTION_CANCEL);
+echo "<br><br>";
 
-echo $testTask->getAvailableActions(Task::STATUS_NEW);
-//echo $testTask->getStatusesMap();
-//echo $testTask->getActionsMap();
+echo "Проверка: getAvailableActions(Task::STATUS_IN_PROCESS) <br>Ожидаем: cancel, respond <br> Результат: ";
+var_dump($testTask->getAvailableActions(Task::STATUS_NEW));
+echo "<br>";
+echo "Проверка: getAvailableActions(Task::STATUS_COMPLETED) <br>Ожидаем: bool(false)<br>Результат: ";
+var_dump($testTask->getAvailableActions(Task::STATUS_COMPLETED));
