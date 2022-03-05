c6d601cbae180dbbcc1a2c60e3ecafe8cb674696
Ольга Н. Курбатова <kon@2i.tusur.ru>
2022-03-05 14:42:02 +0700

    изменение нэймспейса

--- models/Task.php
+++ models/Task.php
@@ -1,5 +1,5 @@
 <?php
-namespace businessLogic;
+namespace app\models;
 
 class Task
 {
--- tests/test.php
+++ tests/test.php
@@ -1,16 +1,16 @@
 <?php
-require_once "../vendor/autoload.php";
+use app\models\Task;
 
-$testTask = new businessLogic\Task(503, 354);
+$testTask = new Task(503, 354);
 echo "Проверка: getNewStatus(Task::ACTION_COMPLETE) <br>Ожидаем: completed<br>Результат: ";
-echo $testTask->getNewStatus(businessLogic\Task::ACTION_COMPLETE);
+echo $testTask->getNewStatus(Task::ACTION_COMPLETE);
 echo "<br>";
 echo "Проверка: getNewStatus(Task::ACTION_CANCEL) <br>Ожидаем: cancelled<br>Результат: ";
-echo $testTask->getNewStatus(businessLogic\Task::ACTION_CANCEL);
+echo $testTask->getNewStatus(Task::ACTION_CANCEL);
 echo "<br><br>";
 
 echo "Проверка: getAvailableActions(Task::STATUS_IN_PROCESS) <br>Ожидаем: cancel, respond <br> Результат: ";
-var_dump($testTask->getAvailableActions(businessLogic\Task::STATUS_NEW));
+var_dump($testTask->getAvailableActions(Task::STATUS_NEW));
 echo "<br>";
 echo "Проверка: getAvailableActions(Task::STATUS_COMPLETED) <br>Ожидаем: bool(false)<br>Результат: ";
-var_dump($testTask->getAvailableActions(businessLogic\Task::STATUS_COMPLETED));
+var_dump($testTask->getAvailableActions(Task::STATUS_COMPLETED));
