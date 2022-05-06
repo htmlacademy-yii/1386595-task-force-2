d42b326c193b5e4db4a0720aec34d5e87b20ca47
Ольга Курбатова <kon@2i.tusur.ru>
2022-05-06 12:19:53 +0700

    доработка проверки в методах

--- models/Task.php
+++ models/Task.php
@@ -58,72 +58,62 @@ class Task
     // Возвращает имя статуса, в который перейдёт задание после выполнения действия $action
     public function getNewStatus($action)
     {
-        try {
-            if (!array_key_exists($action, $this->getActionsMap())) {
-                throw new TaskException("Такого действия не существует");
-            }
-
-            switch ($action) {
-                case self::ACTION_CANCEL:
-                    return self::STATUS_CANCELLED;
-                    break;
-                case self::ACTION_RESPOND:
-                    return self::STATUS_IN_PROCESS;
-                    break;
-                case self::ACTION_COMPLETE:
-                    return self::STATUS_COMPLETED;
-                    break;
-                case self::ACTION_REFUSE:
-                    return self::STATUS_FAILED;
-                    break;
-                default:
-                    return false;
-            }
-        } catch (TaskException $e) {
-            echo "TaskException: " . $e->getMessage();
-            die();
+        if (!array_key_exists($action, $this->getActionsMap())) {
+            throw new TaskException("Такого действия не существует");
         }
 
+        switch ($action) {
+            case self::ACTION_CANCEL:
+                return self::STATUS_CANCELLED;
+                break;
+            case self::ACTION_RESPOND:
+                return self::STATUS_IN_PROCESS;
+                break;
+            case self::ACTION_COMPLETE:
+                return self::STATUS_COMPLETED;
+                break;
+            case self::ACTION_REFUSE:
+                return self::STATUS_FAILED;
+                break;
+            default:
+                return false;
+        }
     }
 
     // Определяет список доступных действий в текущем статусе
     public function getAvailableActions($status, $userId, $customerId, $executorId)
     {
-        try {
-            if (!array_key_exists($status, $this->getStatusesMap())) {
-                throw new TaskException("Такого статуса не существует");
-            }
+        if (!array_key_exists($status, $this->getStatusesMap())) {
+            throw new TaskException("Такого статуса не существует");
+        }
 
-            switch ($status) {
-                case self::STATUS_NEW:
-                    $action = new ActionCancel;
-                    if ($action->checkUserRole($userId, $customerId, $executorId)) {
-                        return new ActionCancel;
-                    } else {
-                        return new ActionRespond;
-                    }
-                    break;
-                case self::STATUS_CANCELLED:
-                    return false;
-                    break;
-                case self::STATUS_IN_PROCESS:
-                    $action = new ActionComplete;
-                    if ($action->checkUserRole($userId, $customerId, $executorId)) {
-                        return new ActionComplete;
-                    } else {
-                        return new ActionRefuse;
-                    }
-                    break;
-                case self::STATUS_COMPLETED:
-                    return false;
-                    break;
-                case self::STATUS_FAILED:
-                    return false;
-                    break;
-            }
-        } catch (TaskException $e) {
-            echo "TaskException: " . $e->getMessage();
-            die();
+        switch ($status) {
+            case self::STATUS_NEW:
+                $action = new ActionCancel;
+                if ($action->checkUserRole($userId, $customerId, $executorId)) {
+                    return new ActionCancel;
+                } else {
+                    return new ActionRespond;
+                }
+                break;
+            case self::STATUS_CANCELLED:
+                return false;
+                break;
+            case self::STATUS_IN_PROCESS:
+                $action = new ActionComplete;
+                if ($action->checkUserRole($userId, $customerId, $executorId)) {
+                    return new ActionComplete;
+                } else {
+                    return new ActionRefuse;
+                }
+                break;
+            case self::STATUS_COMPLETED:
+                return false;
+                break;
+            case self::STATUS_FAILED:
+                return false;
+                break;
         }
     }
+
 }
